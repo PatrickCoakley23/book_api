b8f93e7e662644afa5198fc27237a72471d17425
your name <your.name@cyara.com>
2024-05-10 10:01:11 +0100

    Changed collectstatic

--- book_api/settings.py
+++ book_api/settings.py
@@ -164,11 +164,6 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-    
-    
-    # Override static and media URLs in production
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
