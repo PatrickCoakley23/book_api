7d4118de8a137d33efa16de4bb96bce8a37bbaf2
your name <your.name@cyara.com>
2024-05-10 04:10:46 +0100

    Reconfigured the s3 bucket policy

--- book_api/settings.py
+++ book_api/settings.py
@@ -146,7 +146,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000',
     }
 
-    AWS_STORAGE_BUCKET_NAME = 'bookd-django-api'
+    AWS_STORAGE_BUCKET_NAME = 'book-backend-django'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
